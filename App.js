3cde05f4fd1cf76c4704d591c57e8057b02251fc
Jang <jangbel99@gmail.com>
2022-12-10 15:40:39 -0800

    Fetch data

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ColorPalette from './screens/ColorPalette';
+import ColorPaletteModal from './screens/ColorPaletteModal';
 import Home from './screens/Home';
 
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="Color Palette"
+        component={ColorPalette}
+        options={({ route }) => ({ title: route.params.paletteName })}
+      />
+    </MainStack.Navigator>
+  );
+};
+
 const App = () => {
-  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Color Palette"
-          options={({ route }) => ({ title: route.params.paletteName })}
-          component={ColorPalette}
+      <RootStack.Navigator screenOptions={{ presentation: 'modal' }}>
+        <RootStack.Screen
+          name="Main"
+          options={{ headerShown: false }}
+          component={MainStackScreen}
         />
-      </Stack.Navigator>
+        <RootStack.Screen
+          name="ColorPaletteModal"
+          component={ColorPaletteModal}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
---
+++ screens/ColorPaletteModal.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const ColorPaletteModal = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default ColorPaletteModal;
